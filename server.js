2e872ea00c6cc2a90f56cc519dff084bfc4b65dc
Ahmed Nader <ahmednader111@gmail.com>
2021-07-27 03:05:19 +0200

    Added the basic view files and configured each route to render the corresponding view

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.engine(
+    'handlebars',
+    expressHandlebars({
+        defaultLayout: 'main',
+    })
+);
+
+app.set('view engine', 'handlebars');
+
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 app.use((err, req, res, next) => {
     console.log(err.message);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Welcome to Travel Website!');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Travel Website');
+    res.render('about');
 });
 
 app.listen(port, () => {
